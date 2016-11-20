8568fc3e75238e6baa5bb2c1a97e05b79a5dbb61
Michael Dresser <michaelmdresser@gmail.com>
2016-11-19 21:03:15 -0700

    now pulling obama markovs from talk-to-obama and splitting them to make memes

--- obamagenerator.py
+++ obamagenerator.py
@@ -1,6 +1,23 @@
-import memegenerator, os, random
+import memegenerator, os, random, requests
 
-def makememe(topText, bottomText):
+def makememe():
     filename = "./templates/"
     filename += random.choice(os.listdir("./templates"))
-    memegenerator.make_meme(topText, bottomText, filename)
+
+    content = getContent()
+    contentLength = len(content)
+    breakIndex = 0
+    for i in range(contentLength / 2, contentLength):
+        if content[i] == " ":
+            breakIndex = i
+            break
+
+    memegenerator.make_meme(content[:breakIndex], content[breakIndex + 1:], filename)
+
+def getContent():
+    markov = requests.get('http://talk-to-obama.herokuapp.com/chat?size=tweet')
+    markovDictionary = markov.json()
+    content = markovDictionary["content"]
+    return content
+
+makememe()
